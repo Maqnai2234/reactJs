a53a7741eec803963bda5e0da95c142c1e492f80
Alexei <alexeim763@gmail.com>
2018-09-21 17:43:59 -0500

    first Practice

---
+++ src/components/media.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Media extends Component {
+  render (){
+    return (
+      <div>
+        <p>Holiiii</p>
+      </div>
+    )
+  }
+}
+
+export default Media;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import Media from './components/media'
+
+const app = document.getElementById("root");
+
+render(<Media/>,app)
+

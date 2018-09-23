3028404be11ed0b3ba4a3c5aea7cd2b19902ea04
Alexei <alexeim763@gmail.com>
2018-09-23 17:14:06 -0500

    ADd properties2

--- src/components/media.js
+++ src/components/media.js
@@ -2,9 +2,16 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './media.css';
 class Media extends Component {
+  // constructor(props){
+  //   super(props)
+  //   this.handleClick = this.handleClick.bind(this)
+  // }
+  handleClick = (event) => {
+    console.log(this.props.title);
+  }
   render (){
     return (     
-      <div className="Media">
+      <div className="Media" onClick={this.handleClick}>
         <div className="Media-cover">
           <img
             className="Media-image" 

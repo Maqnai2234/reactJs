c87dfbccd21867583142433cd418b4bcb9410999
Alexei <alexeim763@gmail.com>
2018-09-22 21:14:34 -0500

    added first styles

--- src/components/media.js
+++ src/components/media.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
-
 class Media extends Component {
   render (){
-    return (
-      <div>
-        <p>Holiiii</p>
+    const styles = {
+      container: {
+        color: '#44546b',
+        cursor: 'pointer',
+        width: 260,
+        border: '1px solid lightblue'
+      }
+    }
+    return (     
+      <div style={styles.container}>
+        <div>
+          <img 
+            src="" 
+            alt=""
+            width={260}
+            height={160}
+          />
+          <h3>¿Por qué aprender React?... </h3>
+          <p>Alexei Mamani</p>
+        </div>
       </div>
     )
   }
 }
 
 export default Media;
+

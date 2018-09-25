26d5e433881dc6c4567a56769153a7b08cd4a642
Alexei <alexeim763@gmail.com>
2018-09-25 11:41:13 -0500

    add new icons elements

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Media from './media.js';
 import './playlist.css';
+import Play from '../../icons/components/play';
+import Pause from '../../icons/components/pause';
+import FullScreen from '../../icons/components/full-screen';
+import Volumen from '../../icons/components/volumen';
 
 function Playlist(props){
   const medias = props.playlist
   return(
     <div className="Playlist">
+      <Play size={25} color="red"/>
+      <Pause size={25} color="yellow"/>
+      <FullScreen size={25} color="blue"/>
+      <Volumen size={25} color="gray"/>
+      <br></br>
       {
         medias.map((item) => {
           return <Media {...item} key={item.id}/>
